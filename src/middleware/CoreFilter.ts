1e14f6dd49d4da31f994bac2ce3daad7ef093325
joel joseph <joeljva23@gmail.com>
2020-05-28 18:47:58 -0400

    cors2

--- src/middleware/CoreFilter.ts
+++ src/middleware/CoreFilter.ts
@@ -4,7 +4,7 @@ import { NextFunction,Response,Request} from "express";
 
 export function corsFilter(req:Request,res:Response,next:NextFunction){
     res.header('Access-Control-Allow-Origin',`${req.headers.origin}`);
-res.header('Acess-Control-Allow-Headers',`Origin,Content-Type,Accept`);
+res.header('Access-Control-Allow-Headers',`Origin,Content-Type,Accept`);
 res.header('Access-Control-Allow-Credentials','true');
 res.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
 console.log(req.method)
